13b23622da3c88da90c504a415dbdeebb9fb1a70
bohyen <podo031@naver.com>
2024-02-21 12:41:12 +0900

    # Java 기초 문법(입/출력, datatype)

---
+++ src/basic/casting/CastingExample2.java
@@ -0,0 +1,21 @@
+package basic.casting;
+
+public class CastingExample2 {
+
+    public static void main(String[] args) {
+
+      int i = 72;
+      char c = (char) i;
+        System.out.println("c = " + c);
+        
+        double d = 4.98765;
+        int j = (int) d;
+        System.out.println("여러분들의 이번 달 월급 = " + j*1000000);
+        
+        int k = 1000;
+        byte b = (byte) k;
+        System.out.println("b = " + b);
+
+    }
+
+}
