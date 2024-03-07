232f1c7504cd5a1459425b31cb735224db738376
bohyen <podo031@naver.com>
2024-03-07 12:35:35 +0900

    # DVD 관리 프로그램 UserService 기능 구현

--- src/video/ui/AppUi.java
+++ src/video/ui/AppUi.java
@@ -7,7 +7,7 @@ public class AppUi {
 
     private static Scanner sc = new Scanner(System.in);
 
-    public static String(String message) {
+    public static String inputString(String message) {
         System.out.print(message);
         return sc.nextLine();
     }
@@ -19,7 +19,8 @@ public class AppUi {
             num = sc.nextInt();
         } catch (InputMismatchException e) {
             System.out.println("# 올바른 정수 입력값이 아닙니다!");
-            sc.nextLine();
+        } finally {
+            sc.nextLine(); // try에서도(엔터 입력값), catch에서도(쓰레기 문자열 수거) nextLine()이 동작해야 함.
         }
         return num;
     }
@@ -29,13 +30,25 @@ public class AppUi {
     }
 
     // 시작 화면 출력
-    public static void starScreen() {
+    public static void startScreen() {
         System.out.println("\n========= DVD 대여 관리 시스템 =========");
         System.out.println("### 1. 회원 관리 시스템");
         System.out.println("### 2. 대여 주문 관리 시스템");
         System.out.println("### 3. 영화 DVD 관리 시스템");
         System.out.println("### 4. 프로그램 종료");
+        makeLine();
     }
+
+    // 회원관리 시스템 화면 출력
+    public static void userManagementScreen() {
+        System.out.println("\n========= 회원 관리 시스템 =========");
+        System.out.println("### 1. 신규 회원 추가");
+        System.out.println("### 2. 회원 검색");
+        System.out.println("### 3. 회원 탈퇴");
+        System.out.println("### 4. 첫 화면으로 가기");
+        makeLine();
+    }
+
 
 
 
